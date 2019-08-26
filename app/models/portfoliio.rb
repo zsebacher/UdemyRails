641c6536e1f70607a6f1b99f28b041e49e718193
Zach Sebacher <zach.sebacher@integritystl.com>
2019-08-25 19:55:05 -0500

    create data relationship

--- app/models/portfoliio.rb
+++ app/models/portfoliio.rb
@@ -1,2 +1,3 @@
 class Portfoliio < ApplicationRecord
+  validates_presence_of :title, :body, :main_image, :thumbnail
 end
---
+++ db/migrate/20190826004853_add_topic_refence_to_blogs.rb
@@ -0,0 +1,5 @@
+class AddTopicRefenceToBlogs < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :blogs, :topic, foreign_key: true
+  end
+end
