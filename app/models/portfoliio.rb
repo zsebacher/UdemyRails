cdc921e13500c53936ae825fc90d80b6a279380c
Zach Sebacher <zach.sebacher@integritystl.com>
2019-08-27 13:47:23 -0500

    Integrated ensted attributes for portfolio model

--- app/models/portfoliio.rb
+++ app/models/portfoliio.rb
@@ -2,6 +2,8 @@ class Portfoliio < ApplicationRecord
   include Placeholder
   has_many :technologies
   validates_presence_of :title, :body, :main_image, :thumbnail
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attr| attr['name'].blank? }
   # after_initialize runs when the new action is run in the controller
   after_initialize :set_defaults
 
