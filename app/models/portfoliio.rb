8e0cf221787eeda593f835cf33d9e4dfb8dff035
Zach Sebacher <zach.sebacher@integritystl.com>
2019-08-27 12:33:31 -0500

    add defaults for Portfolio items

--- app/models/portfoliio.rb
+++ app/models/portfoliio.rb
@@ -1,10 +1,18 @@
 class Portfoliio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumbnail
+  # after_initialize runs when the new action is run in the controller
+  after_initialize :set_defaults
 
   # create a customer scope. 2 different formats
   def self.angular
     where(subtitle: "Angular")
   end
 
-  scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails" }
+  scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600X400" # ||= is the or equal operator, if it is already has a value it ignores it
+    self.thumbnail ||= "http://placehold.it/150x150"
+  end
+  
 end
